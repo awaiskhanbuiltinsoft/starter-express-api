8bce086133a1e4094f39b9c0c94e424df51478c2
awaiskhanbuiltinsoft <123465023+awaiskhanbuiltinsoft@users.noreply.github.com>
2023-05-26 16:25:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require("mongoose");
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo Yo Honey Singh!')
 })
+
+(async () => {
+  try {
+    const connect = await mongoose.connect('mongodb+srv://awais:pm4jdDQW0hUOGMPq@cluster0.6eh4ljy.mongodb.net/fyce-app', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log(`DataBase Connected at ${connect.connection.host}`);
+  } catch (error) {
+    console.log("DataBase connection error");
+    process.exit(1);
+  }
+})();
+
+
+
 app.listen(process.env.PORT || 3000)
